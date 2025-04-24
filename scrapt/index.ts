d2df10824e77cb9343262be502c7151959c63eb5
ozan-fn <23SA11A032@students.amikompurwokerto.ac.id>
2025-04-24 08:47:16 +0700

    .

--- scrapt/index.ts
+++ scrapt/index.ts
@@ -1,4 +1,4 @@
 async function main() {
-	//
+	// pemanggilan class GMaps disini buat menjalankan scripting;
 }
 main();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import { PrismaClient } from "@prisma/client";
 import express, { type Request, type Response } from "express";
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+const prisma = new PrismaClient(); // Buat crud semua model di prisma
 
 app.use(express.json());
 
