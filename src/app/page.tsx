356b51bbc4cb76e1a28977d8e8ec8cdcc4fc88f1
Astha <iyandabes1@gmail.com>
2025-05-17 23:59:48 +0700

    update & modified: dependencies and section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
-'use client';
+"use client";
 
 import { useEffect } from "react";
+import { motion } from "framer-motion";
+
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import TrendingDestination from "@/components/TrendingDestination";
+import AboutUs from "@/components/AboutUs";
+
+import PolaroidPhotoMbuluk from "@/images/PantaiMbuluk.jpg";
 
 export default function Home() {
   const menuItems = [
     { label: "Home", href: "#home" },
     { label: "Explore", href: "#explore" },
-    { label: "Ask Ai" , href: "#askAi" },
+    { label: "Ask Ai", href: "#askAi" },
     { label: "About", href: "#about" },
   ];
 
   useEffect(() => {
-
     // Smooth scroll to anchor links
     const handleHashChange = () => {
       const { hash } = window.location;
@@ -43,8 +47,29 @@ export default function Home() {
       <Navbar logoText="AitherWay" menuItems={menuItems} />
       <main className="overflow-hidden">
         <Hero />
+
+        {/* Floating Polaroid */}
+        <motion.div
+          initial={{ opacity: 0, y: 60, rotate: 24, scale: 0.9 }}
+          animate={{ opacity: 1, y: 0, rotate: -12, scale: 0.95 }}
+          transition={{ delay: 1.6, duration: 0.7, ease: "easeOut" }}
+          className="absolute z-50 bottom-40 left-40 mx-auto flex justify-start"
+        >
+          <div className="absolute w-[200px] h-[280px] rotate-18 scale-95">
+            <div className="bg-white p-2 shadow-md rounded-md w-full h-full flex items-center justify-center">
+              <img
+                src={PolaroidPhotoMbuluk.src}
+                alt="Polaroid"
+                className="w-full h-full object-cover rounded"
+              />
+            </div>
+          </div>
+        </motion.div>
+        {/* End Floating Polaroid */}
+
         <TrendingDestination />
+        <AboutUs />
       </main>
     </>
   );
-};
+}
--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,8 +1,46 @@
+import { motion } from "framer-motion";
+
+import { Inter } from "next/font/google";
+import { Playwrite_DE_LA } from "next/font/google";
+
+import Person from "@/images/Person.png"
+
+const inter = Inter({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const playwrite = Playwrite_DE_LA({
+  display: "swap",
+});
+
 const AboutUs = () => {
     return (
-        <section>
-            
-        </section>
+        <section id="about-us" className="py-8 container mx-auto px-4">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex gap-2">
+          <div className="size-1/4 grow">
+            <div className="h-full w-full rounded-lg p-4 bg-cover bg-no-repeat bg-center">
+                <img 
+                src={Person.src}
+                className="bg-teal-600 rounded-t-3xl" 
+                 />
+            </div>
+          </div>
+          <div className="size-1/2 grow text-center mb-12">
+            <span
+              className={`text-rose-500 text-xl font-medium ${playwrite.className}`}
+            >
+              About Us
+            </span>
+            <h2 className={`text-3xl font-bold mt-2 ${inter.className}`}>
+              lorem ipsum dolor sit amet consectetur
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
     );
 
 };
