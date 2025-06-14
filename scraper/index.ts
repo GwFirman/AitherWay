109d65665d91ef078c22945c95ea61c1c266582b
ozan-fn <23SA11A032@students.amikompurwokerto.ac.id>
2025-06-15 00:10:17 +0700

    refactor: enhance main function to support multiple provinces and categories with round-robin API key usage

--- scraper/index.ts
+++ scraper/index.ts
@@ -2,9 +2,68 @@ import GMaps from "../lib/gMaps";
 
 async function main() {
 	const gMapsInstance = new GMaps();
-	// Jadikan true kalo mau save ke Database
+	// Jadikan true kalau mau save ke Database
 	const saveToDB = true;
-	await gMapsInstance.getDetails("gunung jawa tengah", saveToDB);
+
+	const categories = ["gunung", "pantai", "pulau", "air terjun", "danau", "sungai", "gua", "hutan", "lembah", "perbukitan", "taman nasional"];
+
+	const provinces = [
+		"jakarta",
+		"banten",
+		"jawa barat",
+		"jawa tengah",
+		"yogyakarta",
+		"jawa timur",
+		"aceh",
+		"sumatera utara",
+		"sumatera barat",
+		"riau",
+		"kepulauan riau",
+		"jambi",
+		"bengkulu",
+		"sumatera selatan",
+		"bangka belitung",
+		"lampung",
+		"bali",
+		"nusa tenggara barat",
+		"nusa tenggara timur",
+		"kalimantan barat",
+		"kalimantan tengah",
+		"kalimantan selatan",
+		"kalimantan timur",
+		"kalimantan utara",
+		"sulawesi utara",
+		"sulawesi tengah",
+		"sulawesi selatan",
+		"sulawesi tenggara",
+		"gorontalo",
+		"maluku",
+		"maluku utara",
+		"papua",
+		"papua barat",
+		"papua pegunungan",
+	];
+
+	// Daftar API keys untuk digunakan dalam round-robin
+	const apiKeys = ["API_KEY_1", "API_KEY_2", "API_KEY_3", "API_KEY_4"];
+
+	let currentKeyIndex = 0;
+
+	// Fungsi untuk mendapatkan API key berikutnya menggunakan round-robin
+	function getNextApiKey() {
+		const apiKey = apiKeys[currentKeyIndex];
+		currentKeyIndex = (currentKeyIndex + 1) % apiKeys.length;
+		return apiKey;
+	}
+
+	for (const prov of provinces) {
+		for (const cat of categories) {
+			const query = `${cat} di ${prov}`;
+			const apikey = getNextApiKey(); // Ambil API key secara bergantian
+			await gMapsInstance.getDetails(query, saveToDB, apikey);
+		}
+	}
 	console.log("sudah done");
 }
+
 main();
