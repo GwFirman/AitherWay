6d156aaa79d9f4a91ccdb372ec0f5a76f5a2d245
Astha <iyandabes1@gmail.com>
2025-06-14 23:15:40 +0700

    add: array in scraper

--- scraper/index.ts
+++ scraper/index.ts
@@ -4,7 +4,52 @@ async function main() {
 	const gMapsInstance = new GMaps();
 	// Jadikan true kalo mau save ke Database
 	const saveToDB = true;
-	await gMapsInstance.getDetails("gunung jawa tengah", saveToDB);
+
+	const categories = ["gunung", "pantai", "pulau", "air terjun", "danau", "sungai", "gua", "hutan", "lembah", "perbukitan", "taman nasional"];
+
+	const provinces = [
+		"jakarta",
+		"banten",
+		"jawa barat",
+		"jawa tengah",
+		"yogyakarta",
+		"jawa timur",
+		"aceh",
+		"sumatera utara",
+		"sumatera barat",
+		"riau",
+		"kepulauan riau",
+		"jambi",
+		"bengkulu",
+		"sumatera selatan",
+		"bangka belitung",
+		"lampung",
+		"bali",
+		"nusa tenggara barat",
+		"nusa tenggara timur",
+		"kalimantan barat",
+		"kalimantan tengah",
+		"kalimantan selatan",
+		"kalimantan timur",
+		"kalimantan utara",
+		"sulawesi utara",
+		"sulawesi tengah",
+		"sulawesi selatan",
+		"sulawesi tenggara",
+		"gorontalo",
+		"maluku",
+		"maluku utara",
+		"papua",
+		"papua barat",
+		"papua pegunungan",
+	];
+
+	for (const prov of provinces) {
+		for (const cat of categories) {
+			const query = `${cat} ${cat} di ${prov}`;
+			await gMapsInstance.getDetails(query, saveToDB);
+		}
+	}
 	console.log("sudah done");
 }
 main();
